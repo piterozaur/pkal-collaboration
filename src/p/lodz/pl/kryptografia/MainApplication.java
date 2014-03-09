e7f787d4db62549eda983bcde70847f29bf8242c
alisowsk <andrzej.lisowski1@gmail.com>
2014-03-09 23:09:26 +0100

    Podstawowa struktura programu.

---
+++ src/p/lodz/pl/kryptografia/MainApplication.java
@@ -0,0 +1,14 @@
+package p.lodz.pl.kryptografia;
+
+/**
+ * 
+ * @author Andrzej Lisowski 171131
+ *
+ */
+public class MainApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
