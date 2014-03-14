1ff98bce72bfe9f4edbdc2543df21ba066391fd8
Piotr Kluch 165436 <165436@edu.p.lodz.pl>
2014-03-14 15:57:09 +0100

    Swing library initialized inside the app

--- src/p/lodz/pl/kryptografia/MainApplication.java
+++ src/p/lodz/pl/kryptografia/MainApplication.java
@@ -3,12 +3,18 @@ package p.lodz.pl.kryptografia;
 /**
  * 
  * @author Andrzej Lisowski 171131
+ * @author Piotr Kluch 165436
  *
  */
+
 public class MainApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+
+		
+		
 	}
 
 }
