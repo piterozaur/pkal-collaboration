f6d5eba9a5f5e3ed82a02ee38b8661262b81a47d
Piotr Kluch 165436 <165436@edu.p.lodz.pl>
2014-03-15 21:11:01 +0100

    Just the view, stable

--- src/p/lodz/pl/kryptografia/MainApplication.java
+++ src/p/lodz/pl/kryptografia/MainApplication.java
@@ -1,6 +1,9 @@
 package p.lodz.pl.kryptografia;
 
+import java.awt.EventQueue;
+
 /**
+ * The very top class.
  * 
  * @author Andrzej Lisowski 171131
  * @author Piotr Kluch 165436
@@ -12,8 +15,16 @@ public class MainApplication {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-
-		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SwingWindow window = new SwingWindow();
+					window.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 		
 	}
 
--- src/p/lodz/pl/kryptografia/SwingWindow.java
+++ src/p/lodz/pl/kryptografia/SwingWindow.java
@@ -1,28 +1,12 @@
 package p.lodz.pl.kryptografia;
 
-import java.awt.EventQueue;
+import javax.swing.*;
+import java.awt.BorderLayout;
 
-import javax.swing.JFrame;
+public class SwingWindow extends JFrame {
 
-public class SwingWindow {
-
-	private JFrame frame;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					SwingWindow window = new SwingWindow();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	JFrame frame;
+	private JTextField textField;
 
 	/**
 	 * Create the application.
@@ -35,6 +19,50 @@ public class SwingWindow {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
+		
+		final JPanel panel = new JPanel();
+		
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setSize(510,470);
+		getContentPane().setLayout(null);
+		
+		JLabel lblKlucz = new JLabel("Klucz");
+		lblKlucz.setBounds(26, 32, 70, 15);
+		getContentPane().add(lblKlucz);
+		
+		JLabel lblSzyfrowanieDeszyfrowanie = new JLabel("Szyfrowanie / Deszyfrowanie");
+		lblSzyfrowanieDeszyfrowanie.setBounds(26, 156, 314, 15);
+		getContentPane().add(lblSzyfrowanieDeszyfrowanie);
+		
+		textField = new JTextField();
+		textField.setBounds(26, 59, 270, 22);
+		getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JButton btnGenerujKlucz = new JButton("Generuj klucz");
+		btnGenerujKlucz.setBounds(318, 57, 154, 25);
+		getContentPane().add(btnGenerujKlucz);
+		
+		JTextPane txtpnTuWpiszTekst = new JTextPane();
+		txtpnTuWpiszTekst.setText("Miejsce na tekst jawny");
+		txtpnTuWpiszTekst.setBounds(26, 181, 446, 87);
+		getContentPane().add(txtpnTuWpiszTekst);
+		
+		JButton btnSzyfruj = new JButton("Szyfruj");
+		btnSzyfruj.setBounds(26, 280, 214, 25);
+		getContentPane().add(btnSzyfruj);
+		
+		JButton btnDeszyfruj = new JButton("Deszyfruj");
+		btnDeszyfruj.setBounds(258, 280, 214, 25);
+		getContentPane().add(btnDeszyfruj);
+		
+		JTextPane txtpnMiejsceNaKryptogram = new JTextPane();
+		txtpnMiejsceNaKryptogram.setText("Miejsce na kryptogram");
+		txtpnMiejsceNaKryptogram.setBounds(26, 317, 446, 87);
+		getContentPane().add(txtpnMiejsceNaKryptogram);
+		
+		
+
+		
 	}
 }
