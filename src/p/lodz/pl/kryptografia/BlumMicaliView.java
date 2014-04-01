d1c5e4d9715e4d5acc90f8eba07c74cb63d2dd22
Piotr Kluch 165436 <165436@edu.p.lodz.pl>
2014-04-01 14:05:38 +0200

    Blum micali view candidate added

--- src/p/lodz/pl/kryptografia/BlumMicaliView.java
+++ src/p/lodz/pl/kryptografia/BlumMicaliView.java
@@ -1,6 +1,13 @@
 package p.lodz.pl.kryptografia;
 
 import javax.swing.*;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import java.awt.Window;
 
 /**
  * BlumMicaliView class is the JFrame window for this view
@@ -11,17 +18,23 @@ public class BlumMicaliView extends JFrame {
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 2L;
+	private static final long serialVersionUID = 1L;
 
 	public JTextField key1;
-	public JTextField key2;
-	public JTextField key3;
 	public JButton btnGenerateKeys;
 	
 	public JTextArea txtrPlaintext;
 	public JTextArea txtrCiphertext;
 	public JButton btnEncrypt;
 	public JButton btnDecrypt;
+
+	public JButton btnOpenKeys;
+	public JButton btnSaveKeys;
+
+	public JButton btnReadPlaintextFile;
+	public JButton btnWritePlaintextFile;
+	public JButton btnReadCiphertextFile;
+	public JButton btnWriteCiphertextFile;
 	
 	public BlumMicaliView() {
 		
@@ -30,52 +43,100 @@ public class BlumMicaliView extends JFrame {
 		//Defaults
     	//Initialize JFrame
 		this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		this.setSize(635,505);
+		this.setSize(698,355);
     	this.setLocation(200,200);
     	this.setVisible(true);
 		getContentPane().setLayout(null);
 		
 		//Initial GUI starts here
-		JLabel lblKlucz = new JLabel("Klucze 1, 2 i 3:");
-		lblKlucz.setBounds(26, 24, 159, 15);
+		JLabel lblKlucz = new JLabel("Klucz Blum-Micali");
+		lblKlucz.setBounds(29, 23, 169, 15);
 		getContentPane().add(lblKlucz);
 		
 		key1 = new JTextField();
-		key1.setBounds(26, 51, 159, 29);
+		key1.setBounds(26, 50, 172, 29);
 		getContentPane().add(key1);
 		key1.setColumns(10);
 		
-		key2 = new JTextField();
-		key2.setColumns(10);
-		key2.setBounds(26, 92, 159, 29);
-		getContentPane().add(key2);
-		
-		key3 = new JTextField();
-		key3.setColumns(10);
-		key3.setBounds(26, 133, 159, 29);
-		getContentPane().add(key3);
-		
-		btnGenerateKeys = new JButton("Generuj klucze");
-		btnGenerateKeys.setBounds(26, 174, 159, 25);
-		getContentPane().add(btnGenerateKeys);
-		
-		txtrPlaintext = new JTextArea();
+		JPanel middlePanel = new JPanel ();
+		middlePanel.setBounds(259, 23, 190, 148);
+	    middlePanel.setBorder ( new TitledBorder ( new EtchedBorder (), "Tekst jawny" ) );
+	    // create the middle panel components
+	    txtrPlaintext = new JTextArea (7, 15);
 		txtrPlaintext.setText("Miejsce na tekst jawny");
-		txtrPlaintext.setBounds(214, 51, 159, 148);
-		getContentPane().add(txtrPlaintext);
+	    txtrPlaintext.setBounds(218, 51, 187, 148);
+	    JScrollPane scroll = new JScrollPane ( txtrPlaintext );
+	    scroll.setBounds(218, 51, 187, 148);
+	    scroll.setVerticalScrollBarPolicy ( ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS );
+	    scroll.setHorizontalScrollBarPolicy ( ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS );
+	    middlePanel.add ( scroll );
+	    getContentPane().add ( middlePanel );
+
 		
-		txtrCiphertext = new JTextArea();
+		JPanel middlePanel2 = new JPanel ();
+		middlePanel2.setBounds(474, 23, 190, 148);
+	    middlePanel2.setBorder ( new TitledBorder ( new EtchedBorder (), "Szyfrogram" ) );
+	    // create the middle panel components
+		txtrCiphertext = new JTextArea(7, 15);
 		txtrCiphertext.setText("Miejsce na szyfrogram");
-		txtrCiphertext.setBounds(397, 51, 159, 148);
-		getContentPane().add(txtrCiphertext);
+		txtrCiphertext.setBounds(428, 51, 188, 148);
+	    JScrollPane scroll2 = new JScrollPane ( txtrCiphertext );
+	    scroll2.setBounds(428, 51, 188, 148);
+	    scroll2.setVerticalScrollBarPolicy ( ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS );
+	    scroll2.setHorizontalScrollBarPolicy ( ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS );
+	    middlePanel2.add ( scroll2 );
+	    getContentPane().add ( middlePanel2 );
+	    
+
 		
 		btnEncrypt = new JButton("Szyfruj ->");
-		btnEncrypt.setBounds(214, 211, 159, 25);
+		btnEncrypt.setBounds(259, 183, 190, 25);
 		getContentPane().add(btnEncrypt);
 		
 		btnDecrypt = new JButton("<- Deszyfruj");
-		btnDecrypt.setBounds(397, 211, 159, 25);
+		btnDecrypt.setBounds(474, 183, 190, 25);
 		getContentPane().add(btnDecrypt);
+		
+		btnOpenKeys = new JButton("Wczytaj klucz");
+		btnOpenKeys.setBounds(26, 229, 172, 25);
+		getContentPane().add(btnOpenKeys);
+		
+		btnSaveKeys = new JButton("Zapisz klucz");
+		btnSaveKeys.setBounds(26, 266, 172, 25);
+		getContentPane().add(btnSaveKeys);
+		
+		btnReadPlaintextFile = new JButton("Wczytaj plik jawny");
+		btnReadPlaintextFile.setBounds(259, 229, 190, 25);
+		getContentPane().add(btnReadPlaintextFile);
+		
+		btnWritePlaintextFile = new JButton("Zapisz plik jawny");
+		btnWritePlaintextFile.setBounds(259, 266, 190, 25);
+		getContentPane().add(btnWritePlaintextFile);
+		
+		btnReadCiphertextFile = new JButton("Wczytaj plik szyfrowany");
+		btnReadCiphertextFile.setBounds(473, 229, 191, 25);
+		getContentPane().add(btnReadCiphertextFile);
+		
+		btnWriteCiphertextFile = new JButton("Zapisz plik szyfrowany");
+		btnWriteCiphertextFile.setBounds(473, 266, 191, 25);
+		getContentPane().add(btnWriteCiphertextFile);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(204, 204, 204));
+		panel.setBounds(0, 0, 229, 322);
+		getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		btnGenerateKeys = new JButton("Generuj klucz");
+		btnGenerateKeys.setBounds(26, 90, 172, 25);
+		panel.add(btnGenerateKeys);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(51, 51, 51));
+		panel_1.setBounds(228, 0, 468, 322);
+		getContentPane().add(panel_1);
+		
+
 		
 	}
 }
--- src/p/lodz/pl/kryptografia/data_encryption_algorithms/BlumMicaliLogic.java
+++ src/p/lodz/pl/kryptografia/data_encryption_algorithms/BlumMicaliLogic.java
@@ -4,6 +4,10 @@ import p.lodz.pl.kryptografia.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JFileChooser;
+
+import javax.swing.*;
+
 public class BlumMicaliLogic implements ActionListener {
 	
 	protected BlumMicaliView theView;
@@ -33,6 +37,14 @@ public class BlumMicaliLogic implements ActionListener {
 		theView.btnGenerateKeys.addActionListener(this);
 		theView.btnEncrypt.addActionListener(this);
 		theView.btnDecrypt.addActionListener(this);
+		
+		theView.btnOpenKeys.addActionListener(this);
+		theView.btnSaveKeys.addActionListener(this);
+		
+		theView.btnReadPlaintextFile.addActionListener(this);
+		theView.btnReadCiphertextFile.addActionListener(this);
+		theView.btnWritePlaintextFile.addActionListener(this);
+		theView.btnWriteCiphertextFile.addActionListener(this);
 
 	}
 	
@@ -46,7 +58,7 @@ public class BlumMicaliLogic implements ActionListener {
     	//Generate keys
         if ( event.getSource() == theView.btnGenerateKeys ) {
             
-        	setKeys(generateKey_1(), generateKey_2(), generateKey_3());
+        	setKeys(generateKey_1());
         	
         }
     	
@@ -71,23 +83,87 @@ public class BlumMicaliLogic implements ActionListener {
         	String text = getCiphertextTextarea();
         	
         	//TODO Magic happens here
-        	text = "IT SHOULD BE ENCRYPTED NOW.";
+        	text = "IT SHOULD BE DECRYPTED NOW.";
         	
         	//Set ciphertext
         	setPlaintextTextarea ( text );
 
         }
+        
+        if ( event.getSource() == theView.btnOpenKeys ) {
+        	
+        	//New FileChooser open
+        	FileChooser fc = new FileChooser();
+        	String returnValue[] = fc.openDialogKeys();
+        	//System.out.print(returnValue[0]);
+        	
+        	//Assign keys
+        	setKeys(returnValue[0]);
+        	
+        }
+        
+        if ( event.getSource() == theView.btnSaveKeys ) {
+        	
+        	//Get keys
+        	String saveBuffer = theView.key1.getText();
+        	
+        	//New FileChooser save
+        	FileChooser fc = new FileChooser();
+        	fc.saveDialogKeys(saveBuffer.getBytes());
+        	
+        }
+        
+        if ( event.getSource() == theView.btnReadPlaintextFile ) {
+        	
+        	//Choose file
+        	FileChooser fc = new FileChooser();
+        	String returnValue = fc.openDialog();
+        	
+        	//Set textarea
+        	setPlaintextTextarea(returnValue);
+        	
+        }
+        
+        
+        if ( event.getSource() == theView.btnReadCiphertextFile ) {
+        	
+        	//Choose file
+        	FileChooser fc = new FileChooser();
+        	String returnValue = fc.openDialog();
+        	
+        	//Set textarea
+        	setCiphertextTextarea(returnValue);
+        	
+        }
 
+        if ( event.getSource() == theView.btnWritePlaintextFile ) {
+        	
+        	//Get textarea
+        	String saveBuffer = getPlaintextTextarea();
+        	
+        	//Write to file
+        	FileChooser fc = new FileChooser();
+        	fc.saveDialogKeys(saveBuffer.getBytes());        	
+        }
+        
+        if ( event.getSource() == theView.btnWriteCiphertextFile ) {
+        	
+        	//Get textarea
+        	String saveBuffer = getCiphertextTextarea();
+        	
+        	//Write to file
+        	FileChooser fc = new FileChooser();
+        	fc.saveDialogKeys(saveBuffer.getBytes());        	
+        }
+        
     }
 
 	
 	/**
 	 * All getters and setters for current view components
 	 */
-	public void setKeys(String value_1, String value_2, String value_3) {
+	public void setKeys(String value_1) {
 		theView.key1.setText( value_1 );		
-		theView.key2.setText( value_2 );		
-		theView.key3.setText( value_3 );	
 	}
 	private void setCiphertextTextarea(String string) {
 		theView.txtrCiphertext.setText(string);
@@ -109,17 +185,7 @@ public class BlumMicaliLogic implements ActionListener {
 	 */
 	public String generateKey_1() {
 
-		return "0123456789ABCDEF";
-		
-	}
-	public String generateKey_2() {
-
-		return "1133557799BBDDFF";
-		
-	}
-	public String generateKey_3() {
-
-		return "0022446688AACCEE";
+		return "BLUM_MICALI_GENERATED";
 		
 	}
 	
